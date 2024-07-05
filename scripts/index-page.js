2b17c54ddd579c294c140b95b916a273f0f0ca66
JuanDavidDV <juandadazav@gmail.com>
2024-07-05 18:18:38 -0400

    scrips.js created

---
+++ scripts/index-page.js
@@ -0,0 +1,26 @@
+let comments = [
+    {
+        userName: "Victor Pinto",
+        timeStamp: "11/02/2023",
+        content: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        userName: "Christina Cabrera",
+        timeStamp: "10/28/2023",
+        content: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+    },
+    {
+        userName: "Isaac Tadesse",
+        timeStamp: "10/20/2023",
+        content: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    }
+]
+
+let commentInfo = document.querySelector(".comments__container__comment-section__information");
+
+let currentComment = (userComment) => {
+    console.log(comments[userComment]);
+}
+
+currentComment(0);
+
