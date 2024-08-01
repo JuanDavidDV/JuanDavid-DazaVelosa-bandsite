781726c690e9f27c9a2aaed7fab4d1f6a3f69c63
JuanDavidDV <juandadazav@gmail.com>
2024-08-01 15:19:09 -0400

    added api key as a parameter only in the class

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -2,26 +2,24 @@ const endpointUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/comme
 const key = "e0eea5f0-0f8c-4b54-9fc4-ff50843766d4";
 
 class BandSiteApi {
-    constructor(baseUrl, apiKey) {
-        this.baseUrl = baseUrl;
+    constructor(apiKey) {
         this.apiKey = apiKey;
+        this.baseUrl = endpointUrl;
     }
 
     
 }
 
 //Returns 3 default comments
-async function getData() {
+const getComments = async () => {
     try {
         const result = await axios.get(endpointUrl);
         const data = result.data;
-        return console.log(data);
-    } catch(error) {
-        console.log(error)
+        console.log(data);
     }
-}
+    catch(error) {
+        console.log(error);
+    }
+};
 
-getData();
-
-const getAxios = axios.get(endpointUrl);
-console.log(getAxios);
+getComments();
